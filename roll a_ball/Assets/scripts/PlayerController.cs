8edaacad094400e253fdaaff244d2c08c968c7ba
leohafsa <fa19-rcs-021@student.comsats.edu.pk>
2020-07-07 13:01:06 +0500

    done
    done

---
+++ roll a_ball/Assets/scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Text;
+using System;
+using JetBrains.Annotations;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText1;
+    public Text palin;
+    public Text countText;
+    private int count;
+    private Rigidbody rb;
+    private AudioSource source;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+        count = 0;
+        SetCountText();
+
+        countText1.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        string palString = other.GetComponentInChildren<TextMesh>().text;
+        bool flg = IsPalindrome(palString);
+        if (flg.Equals(true) && other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            palin.text = "eaten palindrom:" + count.ToString();
+            SetCountText();
+            source.Play();
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Total Palindrome: " + count.ToString();
+
+        if (count >= 3)
+        {
+
+            countText1.text = "Maximum Collected Palindromes Successfully are:" + count.ToString();
+
+        }
+
+    }
+
+    public bool IsPalindrome(string string1)
+    {
+        char[] ch = string1.ToCharArray();
+        Array.Reverse(ch);
+        string rev = new string(ch);
+        bool b = string1.Equals(rev, StringComparison.OrdinalIgnoreCase);
+        if (b == true)
+        {
+
+            return true;
+        }
+        
+        
+
+            return false;
+        
+
+
+    }
+}
---
+++ roll a_ball/Assets/scripts/SpController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+public class SpController : MonoBehaviour
+{
+    public Vector3 value;
+    public float mWait;
+    public float wait;
+    public float lWait;
+    public bool stop;
+    public int sWait;
+    public GameObject[] collectibles;
+    public int collectiblecount;
+    int randomcollectible;
+    public TextMesh Text;
+    private static int palindromeLength;
+    private string randomString;
+    public int thestringlength;
+    public int countPalindrome;
+    public Text palindrometext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(wSpawn());
+        Text = GameObject.Find("Pick Up").GetComponentInChildren<TextMesh>();
+        Text.text = " ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wait = Random.Range(lWait, mWait);
+        palindromeLength = Random.Range(3, 10);
+        spawn();
+    }
+
+    IEnumerator wSpawn()
+    {
+        yield return new WaitForSeconds(sWait);
+
+        while (collectiblecount < 10)
+        {
+
+
+            randomcollectible = Random.Range(0, 2);
+
+            Vector3 positionofSpawn = new Vector3(Random.Range(-value.x, value.x), 1, Random.Range(-value.z, value.z));
+            Instantiate(collectibles[randomcollectible], positionofSpawn + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(wait);
+            collectiblecount = collectiblecount + 1;
+        }
+    }
+    public void spawn()
+    {
+        int randomNumber;
+        countPalindrome = 0;
+
+
+        randomNumber = Random.Range(0, 3);
+        randomString = "";
+
+        string[] characters = new string[] { "x", "h", "1" };
+        if (randomNumber == 1)
+        {
+            Text.text = GeneratePalindrome(characters);
+            countPalindrome = countPalindrome + 1;
+            palindrometext.text = countPalindrome.ToString();
+
+        }
+        else
+        {
+            Text.text = GenerateRandomString(characters);
+        }
+    }
+
+
+    string GeneratePalindrome(string[] s)
+    {
+        randomString = "";
+
+        thestringlength = Random.Range(9, 15);
+
+        for (int m = 0; m < thestringlength / 2; m++)
+        {
+            randomString = randomString + s[Random.Range(0, s.Length)];
+        }
+        randomString = randomString + new string(randomString.Reverse().ToArray());
+        
+
+
+        return "x" + randomString + "x";
+
+
+    }
+
+    string GenerateRandomString(string[] s)
+    {
+        randomString = "";
+
+        thestringlength = Random.Range(9, 15);
+
+        for (int m = 0; m < thestringlength; m++)
+        {
+            randomString = randomString + s[Random.Range(0, s.Length)];
+        }
+       
+
+        return randomString;
+
+    }
+
+
+}
